b8c456b39a82790bfe1e3aedf3af9fcf49fe92a4
Rachel Patterson <rachelpatterson@Thornes-MacBook-Air.local>
2016-02-18 20:51:09 -0500

    create basic CRUD functions

--- hangman/app/controllers/games_controller.rb
+++ hangman/app/controllers/games_controller.rb
@@ -1,4 +1,31 @@
 class GamesController < ApplicationController
   def index
   end
+
+  # def new
+  #   @game = Game.new
+  # end
+
+  def create
+    @game = Game.new
+  end
+
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def delete
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to root_path
+  end
+
 end
