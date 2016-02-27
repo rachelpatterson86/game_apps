e174cf171ca6f14dd5ea0d4586df70d3ccf48c5c
Rachel Patterson <rachelpatterson@Thornes-MacBook-Air.local>
2016-02-27 14:33:06 -0500

    user can initiate a game; answer to game can be stored into the gaems DB

--- hangman/app/controllers/games_controller.rb
+++ hangman/app/controllers/games_controller.rb
@@ -2,23 +2,15 @@ class GamesController < ApplicationController
   def index
   end
 
-  # def new
-  #   @game = Game.new
-  # end
-
   def create
-    @game = Game.new
-    @game.save
+    word = Game.select_word
+    @game = Game.create(answer: word)
     redirect_to @game
   end
 
-  # def edit
-  #   @game = Game.find(params[:id])
-  # end
-
   def update
     @game = Game.find(params[:id])
-    game_param = params.require(:game).permit(:guess) 
+    game_param = params.require(:game).permit(:guess)
     @game.update(game_param)
     redirect_to @game
   end
--- hangman/app/models/game.rb
+++ hangman/app/models/game.rb
@@ -1,3 +1,22 @@
 class Game < ActiveRecord::Base
   serialize :guess,Array
+
+  def self.select_word
+    words = []
+    word = ""
+    File.open('lib/english-dict.txt','r').each do |f|
+      words << f.delete!("\n")
+    end
+    word = words.sample
+  end
+
+    #add default values... turns 6
+    # :answer - word function
+    #(put below in db/migrate?)
+    # :turns - default value is 6 (put that in DB/migrate)
+    # :guess - initialize as nil (put in db?)
+    # :state - initialize as in progress, only options in progress, completed, paused
+
+
+
 end
