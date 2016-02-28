63a3737c30f3ccd0a47cc1e4767f3685cac02f8b
Rachel Patterson <rachelpatterson@Thornes-Air.att.net>
2016-02-28 02:01:12 -0500

    allows player to make a move and insert the data into :guess

--- hangman/app/controllers/games_controller.rb
+++ hangman/app/controllers/games_controller.rb
@@ -8,10 +8,18 @@ class GamesController < ApplicationController
     redirect_to @game
   end
 
+#TODO:
   def update
     @game = Game.find(params[:id])
-    game_param = params.require(:game).permit(:guess)
-    @game.update(game_param)
+    #game_param = params.require(:game).permit(:guess)
+#need to know where :guess comes from so add :game. Without it, :guess is nil.
+#cant do @game.update(guess: << params[:guess]) b/c << not a method for this.
+# ex to show how the data is passed, thus having to call params this way.
+#   game: {
+  #   guess: 'x'
+  # }
+    @game.guess << params[:game][:guess] #game_param
+    @game.save
     redirect_to @game
   end
 
